29485ab909cf2c137cdce8d69d8566c09fc039c6
Git <ing.yfhl13@outlook.com>
2020-07-16 21:24:43 -0500

    login avanzado

--- app/src/main/java/com/example/fresa/SplashActivity.java
+++ app/src/main/java/com/example/fresa/SplashActivity.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
         Objects.requireNonNull(getSupportActionBar()).hide();
     }
 }
